8f61d58e36cd4fda6419946a1191fd5e06fc06e0
dave <dave@cascadepixels.com.au>
2016-03-03 13:53:48 +1100

    Hiphouse resynced with Murray's work

---
+++ hiphouse/Scripts/FadeOut.js
@@ -0,0 +1,10 @@
+﻿$(".fadeout-container").next().height($(".fadeout-container").height())
+$(window).scroll(function () {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > 100) {
+        $('div.fadeout-container').css('opacity', 0);
+    }
+    else {
+        $('div.fadeout-container').css('opacity', 1 - scrollTop / 100);
+    }
+}).scroll()
